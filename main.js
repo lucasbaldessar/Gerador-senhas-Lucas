ad3e7840b7c219168c0284da09de1fc9f8841e70
lucas_baldessar <144919821+lucasbaldessar@users.noreply.github.com>
2025-08-26 08:41:21 -0300

    g

--- main.js
+++ main.js
@@ -9,10 +9,10 @@ botoes[1].onclick = aumentaTamanho;
 
 function diminuiTamanho() {
     if (tamanhoSenha > 1) {
-        tamanhoSenha = tamanhoSenha - 1
+        tamanhoSenha = tamanhoSenha - 1;
     }
     numeroSenha.textContent = tamanhoSenha;
-    geraSenha()
+    geraSenha();
 }
 
 function aumentaTamanho() {
@@ -20,22 +20,22 @@ function aumentaTamanho() {
         tamanhoSenha = tamanhoSenha + 1;
     }
     numeroSenha.textContent = tamanhoSenha;
-    geraSenha()
+    geraSenha();
 }
 
 const campoSenha = document.querySelector('#campo-senha');
 
-const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz'
-const numeros = '0123456789'
-const simbolos = '@#$%¨&*!?^~+=<>_-'
-geraSenha()
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
+const numeros = '0123456789';
+const simbolos = '@#$%¨&*!?^~+=<>_-';
+geraSenha();
 
 function geraSenha() {
     let senha = '';
     for (let i = 0; i < tamanhoSenha; i++) {
         let numeroAleatorio = Math.random() * letrasMaiusculas.length;
-        numeroAleatorio = Math.floor(numeroAleatorio)
+        numeroAleatorio = Math.floor(numeroAleatorio);
         senha = senha + letrasMaiusculas[numeroAleatorio];
     }
     campoSenha.value = senha;
