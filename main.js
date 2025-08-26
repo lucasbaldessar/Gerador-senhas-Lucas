358f75d9980584a6db636abe489e40b61c09e30e
lucas_baldessar <144919821+lucasbaldessar@users.noreply.github.com>
2025-08-26 07:46:44 -0300

    .

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 const numeroSenha = Document.querySelector('parametro-senha__texto');
-let tamanhoSenha = 12;]
+let tamanhoSenha = 12;
 numeroSenha.textContent = tamanhoSenha;
 
 const botoes = document.querySelectorAll('.parametro-senha__botao');
 
 botoes[0].onclick = diminuiTamanho;
-botoes{ 1 }.onclick = aumentaTamanho;
+botoes[1].onclick = aumentaTamanho;
 
-function diminuiTamanho() {
-    if (tamanhoSenha > 1) {
-        tamanhoSenha = tamanhoSenha - 1{
-        }
-        numeroSenha.textContent = tamanhoSenha;
+function diminuiTamanho(){
+    if (tamanhoSenha > 1){
+        tamanhoSenha = tamanhoSenha - 1
     }
+    numeroSenha.textContent = tamanhoSenha;
+}
