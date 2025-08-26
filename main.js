cbe4535e18be37a0f7edabaa7b60d53f85bd113b
lucas_baldessar <144919821+lucasbaldessar@users.noreply.github.com>
2025-08-26 08:06:39 -0300

    .

--- main.js
+++ main.js
@@ -7,9 +7,16 @@ const botoes = document.querySelectorAll('.parametro-senha__botao');
 botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
 
-function diminuiTamanho(){
-    if (tamanhoSenha > 1){
+function diminuiTamanho() {
+    if (tamanhoSenha > 1) {
         tamanhoSenha = tamanhoSenha - 1
+    }
+    numeroSenha.textContent = tamanhoSenha;
+}
+
+function aumentaTamanho() {
+    if (tamanhoSenha < 20) {
+        tamanhoSenha = tamanhoSenha + 1;
     }
     numeroSenha.textContent = tamanhoSenha;
 }
