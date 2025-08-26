1a275af826896191591c90b23b6b9dd050dadb68
lucas_baldessar <144919821+lucasbaldessar@users.noreply.github.com>
2025-08-26 08:32:05 -0300

    rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr

--- main.js
+++ main.js
@@ -32,6 +32,8 @@ function geraSenha(){
     let senha = '';
     for (let i=0; i<tamanhoSenha; i++){
         let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+        numeroAleatorio = Math.floor(numeroAleatorio)
+        senha = senha + letrasMaiusculas[numeroAleatorio];
     }
 }
 
