533ff973a143f200a8a8cd2db2b618d2b540de3b
lucas_baldessar <144919821+lucasbaldessar@users.noreply.github.com>
2025-09-09 10:36:23 -0300

    b

--- main.js
+++ main.js
@@ -75,7 +75,7 @@ function classificaSenha() {
     } else if (tamanhoSenha > 5 && tamanhoSenha < 12) {
         forcaSenha.classList.add('media');
     } else if (tamanhoSenha <= 5) {
-        forcaSenha.classList.add('feaca');
+        forcaSenha.classList.add('fraca');
     }
-    
+
 }
