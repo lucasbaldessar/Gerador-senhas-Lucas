d80baa8494541eb4078452dd936e65ab939b7452
lucas_baldessar <144919821+lucasbaldessar@users.noreply.github.com>
2025-09-16 11:33:04 -0300

    mm

--- main.js
+++ main.js
@@ -66,16 +66,22 @@ function geraSenha() {
     campoSenha.value = senha;
     classificaSenha();
 }
+let entropia = tamanhoSenha*Math.log2(tamanhoSenha);
 
 function classificaSenha() {
     let entopia = tamanhoSenha*Math.log2(tamanhoSenha);
     forcaSenha.classList.remove('fraca', 'media', 'forte');
-    if (tamanhoSenha > 11) {
+    if (entropia > 57) {
         forcaSenha.classList.add('forte');
-    } else if (tamanhoSenha > 5 && tamanhoSenha < 12) {
+    } else if (entopia > 35 && entropia < 57) {
         forcaSenha.classList.add('media');
-    } else if (tamanhoSenha <= 5) {
+    } else if (entropia <= 35) {
         forcaSenha.classList.add('fraca');
     }
+
+    const valorEntropia = document.querySelecto('.entropia');
+    valorEntropia.textContent = "Um computador pode levar até"
+    Math.floor(2**entropia/(100e6*60*60*24)) + " dias para descobrir essa senha.";
+
 
 }
